48c925f9b4d7e71a9fb4a301f40e839103f83217
green-ogre <balln13572@gmail.com>
2024-08-24 21:45:59 -0600

    nothing

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -7,18 +7,24 @@ public class Player : MonoBehaviour
 
 	// private CharacterController2D controllerRef;
 
+	public Animator animator;
+	private Rigidbody2D rigidBody;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		// controllerRef = GetComponent<CharacterController2D>();
+		// animator.SetFloat("speed", 2.0);
+        rigidBody = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
-	void Update()
+	void LateUpdate()
 	{
 		// float horz = Input.GetAxisRaw("Horizontal");
 		// bool jump = Input.GetAxis("Jump") > 0;
 
 		// controllerRef.Move(horz, jump);
+		animator.SetBool("isRunning", rigidBody.linearVelocity.x > 0f);
 	}
 }
